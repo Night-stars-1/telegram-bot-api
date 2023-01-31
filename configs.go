ed297a3e80010327dfb4b0cab35947bf7d45a41d
Night-stars-1 <99261160+Night-stars-1@users.noreply.github.com>
2023-01-31 23:56:07 +0800

    Update configs.go

--- configs.go
+++ configs.go
@@ -13,9 +13,9 @@ import (
 const (
 	// APIEndpoint is the endpoint for all API methods,
 	// with formatting for Sprintf.
-	APIEndpoint = "https://api.telegram.org/bot%s/%s"
+	APIEndpoint = "https://tg-bot-api.tssaltan.top/bot%s/%s"
 	// FileEndpoint is the endpoint for downloading a file from Telegram.
-	FileEndpoint = "https://api.telegram.org/file/bot%s/%s"
+	FileEndpoint = "https://tg-bot-api.tssaltan.top/file/bot%s/%s"
 )
 
 // Constant values for ChatActions
